184e4299327d140d4ef556a629b65e21bdfae1c8
Hoàng Giang <53357086+HVgiang86@users.noreply.github.com>
2021-01-20 13:45:42 +0700

    Add files via upload

--- amendTheSentence.c
+++ amendTheSentence.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-char* amendTheSentence(char* s)
+char* amendTheSentence(char *s)
 {
     int k=0;
     char tg[100000];
+    if(s[0]>='A' && s[0]<= 'Z'){
+        s[0]+=32;
+    }
     for(int i=0; i<strlen(s); i++){
         if(s[i] >= 'A' && s[i] <= 'Z'){
             tg[k]= ' ';
@@ -18,12 +21,18 @@ char* amendTheSentence(char* s)
         }
     }
     tg[k]='\0';
-    return tg;
+    strcpy(s,tg);
+    return s;
+    //return tg;
 }
 int main(){
     char s[100000];
     scanf("%s",s);
     printf("%s\n",s);
-    printf("%s",amendTheSentence(s));
+    char result[100000];
+//    strcpy(result,amendTheSentence(s));
+//    puts(result);
+puts(amendTheSentence(s));
+    return 0;
 
 }
